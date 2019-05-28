24e6f23ca47c2efb4722db695f45a711fcab4ffa
Chuyang Zhang <chuyangz@usc.edu>
2019-05-27 19:05:53 -0700

    Add files via upload
    2019/05/27 Works.

---
+++ src/components/homepage/DescriptionItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionItem.css'
+
+class DescriptionItem extends React.Component{
+    render() {
+        return this.props.descrips.map((descrips) => (
+            <div className="four wide column">
+                <div>
+                    < img height="100px" src={descrips.img} alt="No pict" />
+                </div>
+                <div>{descrips.sentence}</div>
+
+            </div>
+        ));
+    }
+}
+
+export default DescriptionItem;
