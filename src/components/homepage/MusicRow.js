4ea385c99096b1f23619410ee9de3f5081824bfe
Zack <hzkzqxx@163.com>
2021-04-08 21:26:37 -0700

    init

--- src/components/homepage/MusicRow.js
+++ src/components/homepage/MusicRow.js
@@ -16,49 +16,73 @@ import myImg from './img/IMG_3138.jpg';
 
 class MusicRow extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.next = this.next.bind(this);
         this.previous = this.previous.bind(this);
     }
-    state={
-        artists : [
-            {name:"singer1 singer2 singer3gjklsdgnlsfdsgsfdh",id:1,
-                song:"song title",
-                image:myImg},
-            {name:"singer2",id:2,
-                song:"song title",
-                image:singer2},
-            {name:"singer3",id:3,
-                song:"song title",
-                image:singer3},
-            {name:"singer4",id:4,
-                song:"song title",
-                image:singer4},
-            {name:"singer5",id:5,
-                song:"song title",
-                image:singer5},
-            {name:"singer6",id:6,
-                song:"song title",
-                image:singer6},
-            {name:"singer7",id:7,
-                song:"song title",
-                image:singer1},
-            {name:"singer7",id:8,
-                song:"song title",
-                image:singer2},
-            {name:"singer7",id:9,
-                song:"song title",
-                image:singer3},
-            {name:"singer7",id:10,
-                song:"song title",
-                image:singer6},
-            {name:"singer7",id:11,
-                song:"song title",
-                image:singer6},
-            {name:"singer7",id:12,
-                song:"song title",
-                image:singer6}],
+    state = {
+        artists: [
+            {
+                name: "singer1 singer2 singer3gjklsdgnlsfdsgsfdh", id: 1,
+                song: "song title",
+                image: myImg
+            },
+            {
+                name: "singer2", id: 2,
+                song: "song title",
+                image: singer2
+            },
+            {
+                name: "singer3", id: 3,
+                song: "song title",
+                image: singer3
+            },
+            {
+                name: "singer4", id: 4,
+                song: "song title",
+                image: singer4
+            },
+            {
+                name: "singer5", id: 5,
+                song: "song title",
+                image: singer5
+            },
+            {
+                name: "singer6", id: 6,
+                song: "song title",
+                image: singer6
+            },
+            {
+                name: "singer7", id: 7,
+                song: "song title",
+                image: singer1
+            },
+            {
+                name: "singer7", id: 8,
+                song: "song title",
+                image: singer2
+            },
+            {
+                name: "singer7", id: 9,
+                song: "song title",
+                image: singer3
+            },
+            {
+                name: "singer7", id: 10,
+                song: "song title",
+                image: singer6
+            },
+            {
+                name: "singer7", id: 11,
+                song: "song title",
+                image: singer6
+            },
+            {
+                name: "singer7", id: 12,
+                song: "song title",
+                image: singer6
+            }],
 
     };
 
@@ -71,7 +95,7 @@ class MusicRow extends React.Component {
     render() {
         var settings = {
             dots: false,
-            arrows:true,
+            arrows: true,
             infinite: true,
             speed: 500,
             slidesToShow: 6,
@@ -79,44 +103,37 @@ class MusicRow extends React.Component {
 
         };
         return this.props.genres.map(genre => (
+            //whole div for each row
             <div className='WholeRow'>
+                {/*div for carousel and left right buttons */}
+                <div className='flex_wraper'>
+                    <button className = "carousel_button carousel_button--left" onClick={this.previous} style={{ border: "none", backgroundColor: "transparent" }}>
+                        <img src="../assets/arrow-left.png" />
+                    </button>
+                    <div style={{ width: '90%' }}>
+                        <div>
+                            <h2 className="row_header">{genre}</h2>
+                        </div>
 
-            {/*<div className='ui divider my_divider'></div>*/}
-
-            <div className='flex_wraper'>
-                <div><button
-                    onClick={this.previous}
-                    style={{border:"none", backgroundColor:"transparent"}}
-                ><i className="angle left icon huge" /></button></div>
-
-                <div  style={{width: '90%'}}>
-                    <div>
-                    <h2 className="row_header">{genre}</h2>
-                    </div>
-
-                    {/*<div className="ui grid" style={{width: '90%', margin: '0 auto'}} >*/}
+                        {/*<div className="ui grid" style={{width: '90%', margin: '0 auto'}} >*/}
                         <Slider ref={c => (this.slider = c)} {...settings}>
-                                {this.state.artists.map((artist,i) =>
-                                    <div key={i} >
-                                        <img className="my_img" src={artist.image} alt="No pict shown" />
-                                        <div className="center_item">{artist.name}</div>
-                                    </div>
-                                    )}
+                            {this.state.artists.map((artist, i) =>
+                                <div key={i} >
+                                    <img className="my_img" src={artist.image} alt="No pict shown" />
+                                    <div className="center_item">{artist.name}</div>
+                                </div>
+                            )}
                         </Slider>
-                    {/*</div>*/}
-                    {/*<Artist artists={this.state.artists}/>*/}
+                        {/*</div>*/}
+                        {/*<Artist artists={this.state.artists}/>*/}
 
+                    </div>              
+                    <button className = "carousel_button carousel_button--right" onClick={this.previous} style={{ border: "none", backgroundColor: "transparent" }}>
+                        <img src="../assets/arrow-right.png" />
+                    </button>
                 </div>
 
-                <div><button
-                    className="button"
-                    onClick={this.next}
-                    style={{border:"none", backgroundColor:"transparent"}}
-                ><i className="angle right icon huge" /></button></div>
-
             </div>
-
-        </div>
 
         ));
     };
