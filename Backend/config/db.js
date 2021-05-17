e636b96f0397b6014a2ab47153d89f5e174aa575
Zack <hzkzqxx@163.com>
2021-05-16 17:12:52 -0700

    change backend

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
+const key = require('./keys.js');
 
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, {
+        mongoose.connect(key.mongoURI, {
             useNewUrlParser: true,
             useCreateIndex: true
-        });
+        })
+        console.log("connected");
     } catch (error) {
         console.error(error.message);
         process.exit(1);
--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,6 @@ const app = express();
 // Connect Database
 connectDB();
 
-
 // Init Middleware
 app.use(express.json({ extended: false }));
 
