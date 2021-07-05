a3bab0e0bca5618f8e356216427097698a137177
Zack <hzkzqxx@163.com>
2021-07-04 18:05:06 -0700

    postbuild

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,18 @@ require("./routes/Music")(app);
 // app.use('/api/profile', require('./routes/api/profile'));
 // app.use('/api/posts', require('./routes/api/posts'));
 
-app.use(express.static(__dirname + "../build"));
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('../build'));
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname + '../build/index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 4080;
 
