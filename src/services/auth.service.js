6bbb3e88ea39ad68ae592db2967278443dc475f4
Zack <hzkzqxx@163.com>
2021-07-05 18:09:07 -0700

    add user accesstoken authentication service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -11,14 +11,25 @@ class AuthService {
                 password: password
             });
     }
-
+    //service for logging in
     logIn(email, password) {
         return API.post("/api/auth/userLogIn", {
             email: email,
             password: password
         }).then(response => {
-            console.log(response.data)
+            if(response.data.accessToken){
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+            return response.data;
         })
+    }
+    //service for logging out
+    logOut(){
+        localStorage.removeItem("user");
+    }
+    //service for getting current user
+    getCurrentUser(){
+        return JSON.parse(localStorage.getItem('user'));
     }
 
     
