55ea3d0d366118fd7c1ffd3ebf4e2fd9f5ceb11c
Zack <hzkzqxx@163.com>
2021-07-23 09:22:32 -0700

    upload dev

---
+++ Backend/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: "mongodb+srv://tuka:tukaMVPprototype@cluster0.ebmgp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    //mongodb+srv://tukaheroku:tukaMVPprototypeheroku@cluster0.kqy2r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+    //secret key to encode and decode token
+    jwtSecret: "mySecretToken"
+};
+
